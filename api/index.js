c3dbf52e2a8b08e82616dc9982144d29ac837cef
Chystiakova Daria <123006049+breadoorr@users.noreply.github.com>
2024-10-15 09:46:19 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'https://chat-app-front-nlj6glls8-ddorabbles-projects.vercel.app',
+    origin: 'https://chat-app-front-orcin.vercel.app',
 }));
 const jwtSecret = process.env.JWT_SECRET;
 const bcryptSalt = bcrypt.genSaltSync(10);
