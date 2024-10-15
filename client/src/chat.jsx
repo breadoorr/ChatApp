0944927ebe22163bf529bdaf4f8c296876d72fc9
Chystiakova Daria <123006049+breadoorr@users.noreply.github.com>
2024-10-15 11:28:29 +0300

    Update chat.jsx

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -20,7 +20,7 @@ export default function Chat() {
     }, []);
 
     function connect() {
-        const ws = new WebSocket('ws://chat-app-steel-two.vercel.app');
+        const ws = new WebSocket('ws://chat-app-front-orcin.vercel.app');
         setWs(ws);
         ws.addEventListener('message', handleMessage);
         ws.addEventListener('close', () => {
